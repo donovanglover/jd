57ff580e8441a9fddd302d92df3206b5bc0256a2
Donovan Glover <donovan@dglover.co>
2023-11-22 16:01:59 -0500

    feat: Add base logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+use cli::{Cli, Commands};
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Add { name }) => {
+            println!("{:?}", name)
+        },
+        Some(Commands::Remove { name }) => {
+            println!("{:?}", name)
+        },
+        Some(Commands::Ls { name }) => {
+            println!("{:?}", name)
+        },
+        None => {
+            println!("none")
+        }
+    }
 }
