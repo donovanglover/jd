5830dfa3694c6d9d753b0ef7cb5e9c6df02bcce3
Donovan Glover <donovan@dglover.co>
2023-12-11 00:52:04 -0500

    feat: Move logic to implementations

--- jd/src/lib.rs
+++ jd/src/lib.rs
@@ -28,151 +28,116 @@ pub struct Id {
     // name: &'static str,
 }
 
-// Strict mode?
-// impl Area {
-//     pub fn from_str(str: &str) -> Result<Area, &str> {
-//     }
-// }
+impl Area {
+    /// TODO: Allow for alternatives to the dash separator and space separator?
+    pub fn from_str(str: &str) -> Result<Self, &'static str> {
+        let chars: Vec<char> = str.chars().collect();
 
-// impl Category {
-//     pub fn new() -> Option<Category> {
-//         Some(Category {
-//             area: 0,
-//             category: 0,
-//         })
-//     }
-// }
-//
-// impl Id {
-//     pub fn from_array(arr: [u8; 2]) -> Option<Id> {
-//         Some(Id {
-//             area: 0,
-//             category: 0,
-//             id: 3,
-//         })
-//     }
-//
-//     pub fn from_string(str: &str) -> Result<Id, &str> {
-//         let chars: Vec<char> = str.chars().collect();
-//
-//         is_valid_acid(chars)?;
-//
-//         Ok(Id {
-//             area: 0,
-//             category: 0,
-//             id: 0,
-//         })
-//         // Ok(Id {
-//         //     area: Area::from_acid_a(chars[0].to_digit(10).unwrap()),
-//         //     category: Category::from_acid_ac(chars[0].to_digit(10).unwrap(), chars[1].to_digit(10).unwrap()),
-//         //     id: chars[3] as u8,
-//         // })
-//     }
-// }
+        if chars.len() < 5 {
+            return Err("Given area is too short to follow a0-a9")
+        }
 
-/// TODO: Allow for alternatives to the dash separator and space separator?
-pub fn get_area(str: &str) -> Result<Area, &'static str> {
-    let chars: Vec<char> = str.chars().collect();
+        if !chars[0].is_ascii_digit() {
+            return Err("Given area does not have valid starting digit a in a0")
+        }
 
-    if chars.len() < 5 {
-        return Err("Given area is too short to follow a0-a9")
+        if chars[1] != '0' {
+            return Err("Given area does not have a starting digit that ends in 0 in a0-a9")
+        }
+
+        if chars[2] != '-' {
+            return Err("Given area does not have dash separator in a0-a9")
+        }
+
+        if chars[3] != chars[0] {
+            return Err("Given area does not have an ending digit equal to a in a0-a9")
+        }
+
+        if chars[4] != '9' {
+            return Err("Given area does not end with 9 in a0-a9")
+        }
+
+        if chars.len() < 7 {
+            return Err("Given area is too short to have a name")
+        }
+
+        if chars[5] != ' ' {
+            return Err("Given area does not have a space separator")
+        }
+
+        Ok(Area {
+            area: chars[0..5].into_iter().collect(),
+            name: chars[6..chars.len()].into_iter().collect(),
+        })
     }
-
-    if !chars[0].is_ascii_digit() {
-        return Err("Given area does not have valid starting digit a in a0")
-    }
-
-    if chars[1] != '0' {
-        return Err("Given area does not have a starting digit that ends in 0 in a0-a9")
-    }
-
-    if chars[2] != '-' {
-        return Err("Given area does not have dash separator in a0-a9")
-    }
-
-    if chars[3] != chars[0] {
-        return Err("Given area does not have an ending digit equal to a in a0-a9")
-    }
-
-    if chars[4] != '9' {
-        return Err("Given area does not end with 9 in a0-a9")
-    }
-
-    if chars.len() < 7 {
-        return Err("Given area is too short to have a name")
-    }
-
-    if chars[5] != ' ' {
-        return Err("Given area does not have a space separator")
-    }
-
-    Ok(Area {
-        area: chars[0..5].into_iter().collect(),
-        name: chars[6..chars.len()].into_iter().collect(),
-    })
 }
 
-pub fn get_category(str: &str) -> Result<Category, &'static str> {
-    let chars: Vec<char> = str.chars().collect();
 
-    if chars.len() < 2 {
-        return Err("Given category is too short to follow [00-99]")
+impl Category {
+    pub fn from_str(str: &str) -> Result<Self, &'static str> {
+        let chars: Vec<char> = str.chars().collect();
+
+        if chars.len() < 2 {
+            return Err("Given category is too short to follow [00-99]")
+        }
+
+        if !chars[0].is_ascii_digit() || !chars[1].is_ascii_digit() {
+            return Err("Given category does not have valid starting digits [00-99]")
+        }
+
+        if chars.len() < 4 {
+            return Err("Given category is too short to have a name")
+        }
+
+        if chars[2] != ' ' {
+            return Err("Given category does not have a space separator")
+        }
+
+        Ok(Category {
+            category: chars[0..2].into_iter().collect(),
+            area: [chars[0], '0', '-', chars[0], '9'].into_iter().collect(),
+            name: chars[3..chars.len()].into_iter().collect(),
+        })
     }
-
-    if !chars[0].is_ascii_digit() || !chars[1].is_ascii_digit() {
-        return Err("Given category does not have valid starting digits [00-99]")
-    }
-
-    if chars.len() < 4 {
-        return Err("Given category is too short to have a name")
-    }
-
-    if chars[2] != ' ' {
-        return Err("Given category does not have a space separator")
-    }
-
-    Ok(Category {
-        category: chars[0..2].into_iter().collect(),
-        area: [chars[0], '0', '-', chars[0], '9'].into_iter().collect(),
-        name: chars[3..chars.len()].into_iter().collect(),
-    })
 }
 
-pub fn get_id(str: &str) -> Result<Id, &'static str> {
-    let chars: Vec<char> = str.chars().collect();
+impl Id {
+    pub fn from_str(str: &str) -> Result<Self, &'static str> {
+        let chars: Vec<char> = str.chars().collect();
 
-    if chars.len() < 5 {
-        return Err("Given id is too short to follow ac.id")
+        if chars.len() < 5 {
+            return Err("Given id is too short to follow ac.id")
+        }
+
+        if !chars[0].is_ascii_digit() {
+            return Err("Given id does not have a valid starting area digit a in ac.id")
+        }
+
+        if !chars[1].is_ascii_digit() {
+            return Err("Given id does not have a valid category ac in ac.id")
+        }
+
+        if chars[2] != '.' {
+            return Err("Given id does not have a decimal separator in ac.id")
+        }
+
+        if !chars[3].is_ascii_digit() || !chars[4].is_ascii_digit() {
+            return Err("Given id does not have valid digits id to follow ac.id")
+        }
+
+        if chars.len() < 7 {
+            return Err("Given id is too short to have a name")
+        }
+
+        if chars[5] != ' ' {
+            return Err("Given id does not have a space separator")
+        }
+
+        Ok(Id {
+            id: chars[0..5].into_iter().collect(),
+            category: chars[0..2].into_iter().collect(),
+            area: [chars[0], '0', '-', chars[0], '9'].into_iter().collect(),
+            name: chars[6..chars.len()].into_iter().collect(),
+        })
     }
-
-    if !chars[0].is_ascii_digit() {
-        return Err("Given id does not have a valid starting area digit a in ac.id")
-    }
-
-    if !chars[1].is_ascii_digit() {
-        return Err("Given id does not have a valid category ac in ac.id")
-    }
-
-    if chars[2] != '.' {
-        return Err("Given id does not have a decimal separator in ac.id")
-    }
-
-    if !chars[3].is_ascii_digit() || !chars[4].is_ascii_digit() {
-        return Err("Given id does not have valid digits id to follow ac.id")
-    }
-
-    if chars.len() < 7 {
-        return Err("Given id is too short to have a name")
-    }
-
-    if chars[5] != ' ' {
-        return Err("Given id does not have a space separator")
-    }
-
-    Ok(Id {
-        id: chars[0..5].into_iter().collect(),
-        category: chars[0..2].into_iter().collect(),
-        area: [chars[0], '0', '-', chars[0], '9'].into_iter().collect(),
-        name: chars[6..chars.len()].into_iter().collect(),
-    })
 }
