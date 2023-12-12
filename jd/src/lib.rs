0c2d636c1b208505c08e64930ff05f7e4badee89
Donovan Glover <donovan@dglover.co>
2023-12-12 14:43:25 -0500

    docs(jd): Add usage instructions

--- jd/src/lib.rs
+++ jd/src/lib.rs
@@ -27,6 +27,16 @@
 //! The use of `String` makes it easy to use this library in both Rust and JavaScript (through
 //! WebAssembly/Wasm) without having to worry about custom types.
 //!
+//! # Usage
+//!
+//! Since I haven't published this crate to `crates.io`, you'll need to source the Git repository
+//! directly.
+//!
+//! ```toml
+//! [dependencies]
+//! jd = { git = "https://github.com/donovanglover/jd.git" }
+//! ```
+//!
 //! # Performance
 //!
 //! This crate uses no external dependencies. All functionality is achieved with Rust's standard
